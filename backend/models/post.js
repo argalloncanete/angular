803fd6f49a0547b7db854700d6f51e4dff85b10b
Luffy <53109860+argalloncanete@users.noreply.github.com>
2021-05-25 12:50:45 +0800

    Delete backend directory

--- backend/models/post.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const postSchema = mongoose.Schema({
-  ordName: { type: String, required: true },
-  ordPrice: { type: String, required: true },
-  discounted: { type: Boolean, default: false },
-});
-
-module.exports = mongoose.model("Post", postSchema);
